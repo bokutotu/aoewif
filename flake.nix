b9eb542f4f5e661c4f5fffa0198fa1669a18923f
Hikaru Kondo <mushin.hudoushin@gmail.com>
2026-08-12 10:30:00 +0900

    Add Nix development environment

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "aoewif Rust development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              cargo
+              clippy
+              rust-analyzer
+              rustc
+              rustfmt
+            ];
+          };
+        }
+      );
+
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt);
+    };
+}
