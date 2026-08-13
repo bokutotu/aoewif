db8ec6771a2ccf3c8c4e18e2010c0c63791424fa
Hikaru Kondo <mushin.hudoushin@gmail.com>
2026-08-13 19:24:53 +0900

    package.yaml

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -1,12 +1,1 @@
-module Main (main) where
-
-import qualified ComputeSpec
-import qualified IntegrationSpec
-import qualified ScheduleSpec
-import           Test.Hspec      (hspec)
-
-main :: IO ()
-main = hspec $ do
-    ComputeSpec.spec
-    ScheduleSpec.spec
-    IntegrationSpec.spec
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
